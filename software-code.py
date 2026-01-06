2e110acdfb46aba6ba3193e63ce8d4af653d3a6f
Frederico <frederico.passos@lccv.ufal.br>
2026-01-06 10:37:26 -0300

    Ajuste do tamanho das fontes e inserção das labels para os eixos e para o fit

--- software-code.py
+++ software-code.py
@@ -13,7 +13,15 @@ from scipy.optimize import curve_fit
 #COMPONENTES DA TELA INICIAL
 
 tela = Tk()
-tela.geometry("700x700")
+is_fullscreen = False
+def toggle_fullscreen(event=None):
+    global is_fullscreen
+    is_fullscreen = not is_fullscreen
+    tela.attributes('-fullscreen', is_fullscreen)
+
+tela.bind('<F11>', toggle_fullscreen)
+tela.bind('<Escape>', lambda e: tela.attributes('-fullscreen', False))
+# tela.geometry("700x700")
 #tela.state('zoomed')
 tela.title("Produto educacional")
 
@@ -64,12 +72,12 @@ def arquivo(a):
 
         plt.figure(figsize=(8, 6))
         plt.errorbar(x_data, y_data, xerr=x_sigma, fmt='o', capsize=4, label='Dados com incerteza em t')
-        plt.plot(x_fit, y_fit, '-', label=f'Ajuste linear: y = {a:.2f}x + {b:.2f}' )
+        plt.plot(x_fit, y_fit, '-', label=f'Ajuste linear: {cx2_1.get()} = {a:.2f}{cx1_1.get()} + {b:.2f}' )
 
 
         plt.title('Ajuste Linear com Desvio Padrão')
-        plt.xlabel('Variável X')
-        plt.ylabel('Variável Y')
+        plt.xlabel(cx1.get())
+        plt.ylabel(cx2.get())
         plt.legend()
         plt.grid(True)
         plt.show()
@@ -77,22 +85,26 @@ def arquivo(a):
     else:
         messagebox.showwarning("Seleção Cancelada", "Nenhum arquivo selecionado.")
             
-lb1=Label(tela, text="Selecione a quantidade de curvas no gráfico", font=("Fixedsys", 18), fg="black", width=50, height=3)
+lb1=Label(tela, text="Selecione a quantidade de curvas no gráfico", font=("Fixedsys", 12), fg="black", width=50, height=3)
 lista1=["1","2","3","4"]
-cb1=ttk.Combobox(tela, values=lista1, font=("Fixedsys", 18), width=5, height=10)
+cb1=ttk.Combobox(tela, values=lista1, font=("Fixedsys", 12), width=5, height=10)
 cb1.set("1")
-lb=Label(tela, text="Selecione o tipo de ajuste (fit)", font=("Fixedsys", 18), fg="black", width=50, height=3)
+lb=Label(tela, text="Selecione o tipo de ajuste (fit)", font=("Fixedsys", 12), fg="black", width=50, height=3)
 lista2=["Linear","Polinomial grau 2"]
-cb=ttk.Combobox(tela, values=lista2, font=("Fixedsys", 18), width=20, height=10)
+cb=ttk.Combobox(tela, values=lista2, font=("Fixedsys", 12), width=20, height=10)
 cb.set("Linear")
-lb2=Label(tela, text="Quais os eixos que foram medidos várias vezes?", font=("Fixedsys", 18), fg="black", width=50, height=3)
-lista3=["x","y", "Ambos"]
-cb2=ttk.Combobox(tela, values=lista3, font=("Fixedsys", 18), width=5, height=10)
-cb2.set("x")
-lb3=Label(tela, text="Qual o rótulo do eixo x?", font=("Fixedsys", 18), fg="black", width=50, height=3)
+lb2=Label(tela, text="Quais os eixos que foram medidos várias vezes?", font=("Fixedsys", 12), fg="black", width=50, height=3)
+lista3=["Apenas x","Apenas y", "Ambos"]
+cb2=ttk.Combobox(tela, values=lista3, font=("Fixedsys", 12), width=10, height=10)
+cb2.set("Apenas x")
+lb3=Label(tela, text="Qual o rótulo do eixo x?", font=("Fixedsys", 12), fg="black", width=50, height=3)
 cx1= Entry(tela)
-lb4=Label(tela, text="Qual o rótulo do eixo y?", font=("Fixedsys", 18), fg="black", width=50, height=3)
+lb3_1=Label(tela, text="E sua sigla?", font=("Fixedsys", 12), fg="black", width=50, height=3)
+cx1_1= Entry(tela)
+lb4=Label(tela, text="Qual o rótulo do eixo y?", font=("Fixedsys", 12), fg="black", width=50, height=3)
 cx2= Entry(tela)
+lb4_1=Label(tela, text="E sua sigla?", font=("Fixedsys", 12), fg="black", width=50, height=3)
+cx2_1= Entry(tela)
 botaofit= Button(tela, text="Escolher arquivo",bg="green",fg="white",width=20, height=2, font=("Fixedsys", 9), command= lambda: arquivo(1))
 
 #Posicionamento dos componentes da tela 3:
@@ -104,8 +116,12 @@ lb2.pack()
 cb2.pack()
 lb3.pack()
 cx1.pack()
+lb3_1.pack()
+cx1_1.pack()
 lb4.pack()
 cx2.pack()
+lb4_1.pack()
+cx2_1.pack()
 
 botaofit.pack(padx=0, pady=10)
 
